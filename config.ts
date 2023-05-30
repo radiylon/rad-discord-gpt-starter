66a24cea17852ebc8120316c967b2a422b554186
radiylon <radiylon@gmail.com>
2023-05-30 00:04:16 -0700

    Clean up

--- config.ts
+++ config.ts
@@ -11,8 +11,6 @@ const config: Config = {
     DISCORD_TOKEN: process.env.DISCORD_TOKEN,
     OPENAI_ORG: process.env.OPENAI_ORG,
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
   },
 };
 
